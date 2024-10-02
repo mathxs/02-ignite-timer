0cdea9dd69365671d708cd4afadca4ef460b76a8
mathxs <matheusdsantosp@gmail.com>
2024-10-02 08:48:17 -0300

    mudança da hianka

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -1,9 +1,9 @@
 import 'styled-components'
 import { defaultTheme } from '../styles/themes/default'
 
-type ThmeType = typeof defaultTheme
+type ThemeType = typeof defaultTheme
 
 declare module 'styled-components' {
   // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  export interface DefaultTheme extends ThmeType {}
+  export interface DefaultTheme extends ThemeType {}
 }
